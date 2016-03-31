36ac2495fe88d578481d9bbf34b9fd2ee86ae6b9
Nick Jennings <nick@silverbucket.net>
2016-03-31 15:49:36 +0200

    more work as I add tests

--- src/index.js
+++ src/index.js
@@ -1,45 +1,59 @@
 var ArrayKeys = require('array-keys');
-var randToken = require('rand-token');
+
+var RESERVED_BYTES = 8;
 
 var files = new ArrayKeys({
     identifier: 'uid'
 });
 
-function BDT(metadata = {}) {
-    var {filename, mimeType, arrayBuffer, chunkSize, uid} = metadata;
-    var totalChunks;
+// we reserve 4 bytes for the UID, so we can have a max number of 65535
+function genUID() {
+    return Math.floor((Math.random() * 65535) + 1);
+}
+
+function BDT(metadata) {
+    if (typeof metadata !== 'object') { throw new Error('metadata object must be passed in'); }
+
+    var totalChunks, payloadSize;
     
-    if (! filename) { throw new Error('no filename specified'); }
-    if (! mimeType) { throw new Error('no mimeType specified'); }
-    if (! chunkSize) { throw new Error('no chunkSize specified'); }
-    if ((arrayBuffer) && 
-       ((typeof arrayBuffer.toString !== 'function') || (arrayBuffer.toString() !== '[object ArrayBuffer]'))) { 
+    if (! metadata.fileName) { throw new Error('no fileName specified'); }
+    if (! metadata.mimeType) { throw new Error('no mimeType specified'); }
+    if (! metadata.chunkSize) { throw new Error('no chunkSize specified'); }
+    if ((metadata.arrayBuffer) && 
+       ((typeof metadata.arrayBuffer.toString !== 'function') || (metadata.arrayBuffer.toString() !== '[object ArrayBuffer]'))) { 
         throw new Error('arrayBuffer must be an actual ArrayBuffer object'); 
-    } else if ((! arrayBuffer) && (! uid)) { 
+    } else if ((! metadata.arrayBuffer) && (! metadata.uid)) { 
         throw new Error('arrayBuffer or uid must be specified');
-    } else if ((arrayBuffer) && (uid)) {
+    } else if ((metadata.arrayBuffer) && (metadata.uid)) {
         throw new Error('cannot manually assign uid');
-    } else if (! uid) {
+    } else if (! metadata.uid) {
         // we have an arrayBuffer, so we create a UID  
-        uid = randToken.uid(4);
-    } else if (arrayBuffer) {
+        metadata.uid = genUID();
+    } else if (metadata.arrayBuffer) {
         // we have an arrayBuffer, so let's calc total number of chunks
-        totalChunks = Math.ceil(arrayBuffer.byteLength / chunkSize);
+        payloadSize = (metadata.chunkSize - RESERVED_BYTES)
+        totalChunks = Math.ceil(metadata.arrayBuffer.byteLength / payloadSize);
     }
 
     this._metadata = {
-        uid: uid,
-        filename: filename,
-        mimeType: mimeType,
-        chunkSize: chunkSize,
-        fileSize: (arrayBuffer) ? arrayBuffer.byteLength : 0,
-        arrayBuffer: (arrayBuffer) ? arrayBuffer : null,
-        totalChunks: (totalChunks) ? totalChunks : 0
+        uid: metadata.uid,
+        fileName: metadata.fileName,
+        mimeType: metadata.mimeType,
+        chunkSize: metadata.chunkSize,
+        payloadSize: payloadSize,
+        reservedSize: RESERVED_BYTES,
+        fileSize: (metadata.arrayBuffer) ? metadata.arrayBuffer.byteLength : 0,
+        totalChunks: (totalChunks) ? totalChunks : (metadata.totalChunks) ? metadata.totalChunks : 0
         // TODO calc checksums for every chunk
     };
     
+    this._arrayBuffer = (metadata.arrayBuffer) ? metadata.arrayBuffer : null,
     this._chunksProcessed = 0;
 }
+
+BDT.prototype.getUID = function () {
+    return this._metadata.uid;
+};
 
 BDT.prototype.onChunkReceived = function () {};
 
@@ -60,17 +74,25 @@ BDT.prototype.getTotalChunks = function () {
 BDT.prototype.getChecksum = function () {};
 
 BDT.prototype.getChunk = function (num) {
-    var start = (this._metadata.chunkSize * (typeof num === 'number') ? num : this._chunksProcessed) - this._metadata.chunkSize;
+    var start = (this._metadata.payloadSize * (typeof num === 'number') ? num : this._chunksProcessed) - this._metadata.payloadSize;
     if (start >= this._metadata.fileSize) {
         return undefined;
     }
-    var end = start + this._chunkSize;
+    var end = start + this._payloadSize;
     end = (end > this._metadata.fileSize) ? this._metadata.fileSize : end;
     
     if (! num) {
         this._chunksProcessed += 1;
     }
     
+    var ab = new ArrayBuffer(this._metadata.chunkSize);
+    var view = new DataView(ab);
+    view.setInt16(0, this._metadata.uid);
+    view.setInt16(4, num);
+    new Uint8Array(ab, 0, ab.byteLength).set(new Uint8Array(this._arrayBuffer.slice(start, end)), 8);
+    
     // TODO add header data before returning chunk
-    return this._metadata.arrayBuffer.slice(start, end);
+    return ab;
 };
+
+module.exports = BDT;
