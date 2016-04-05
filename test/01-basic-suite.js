e7b905b7207d8354fc7661fb33d7c1885cb1639c
Nick Jennings <nick@silverbucket.net>
2016-04-05 15:04:24 +0200

    run tests in browser

--- test/01-basic-suite.js
+++ test/01-basic-suite.js
@@ -6,24 +6,90 @@ define(['require', './FileHelper.js'], function (require, FH) {
         {
             desc: 'basic tests',
             setup: function (env, test) {
-                env.BDT = require('./../src/index');
-                var ab = FH.getFile('test.mp4');
-                env.sender = new env.BDT({
-                    fileName: 'test.mp4',
-                    mimeType: 'video/mp4',
-                    arrayBuffer: ab,
-                    chunkSize: 16000
+                if (typeof BinaryDataChunking === 'function') {
+                    env.BDC = BinaryDataChunking;
+                } else {
+                    env.BDC = require('./../src/index'); 
+                }
+
+                // 16000 chunk size, 67 chunks
+                FH.getFile('test.mp4', function (ab) {
+                    env.sender = new env.BDC({
+                        name: 'test.mp4',
+                        mimeType: 'video/mp4',
+                        arrayBuffer: ab,
+                        chunkSize: 16000
+                    });
+                    env.totalChunks = 67;
+                    test.assertType(env.sender.getMetadata, 'function');
                 });
-                env.totalChunks = env.sender.getTotalChunks();
-                test.assertAnd((env.sender.getUID() < 65536) && (env.sender.getUID() > 0), true);
-                test.assert(env.sender.getFileSize(), 1055736);
             },
             tests: [
                 {
+                    desc: '# verify sender properties',
+                    run: function (env, test) {
+                        test.assertAnd((env.sender.getUID() < 65536) && (env.sender.getUID() > 0), true);
+                        // console.log('UID: ' + env.sender.getUID());
+                        test.assertAnd(env.sender.getChecksum(), 'd55bddf8d62910879ed9f605522149a8');
+                        test.assertAnd(env.sender.getFileSize(), 1055736);
+                        test.assert(env.sender.getTotalChunks(), env.totalChunks);    
+                    }
+                },
+                {
                     desc: '# create receiver',
                     run: function (env, test) {
-                        env.receiver = new env.BDT(env.sender.getMetadata());
+                        env.receiver = new env.BDC(env.sender.getMetadata());
                         test.assert(env.receiver.getTotalChunks(), env.totalChunks);
+                    }
+                },
+                {
+                    desc: '# get first chunk and verify positioning',
+                    run: function (env, test) {
+                        var chunk = env.sender.getChunk();
+                        test.assertAnd(chunk.byteLength, 16000)
+                        env.BDC.submitChunk(chunk);
+                        test.assert(env.sender.getPosition(), 1);      
+                    }
+                },
+                {
+                    desc: '# check receiver position',
+                    run: function (env, test) {
+                        test.assert(env.receiver.getPosition(), 1);
+                    }
+                },
+                {
+                    desc: '# get ordered chunks',
+                    run: function (env, test) {
+                        env.sender.forEachChunk(function (chunk, pos) {
+                            if (pos < 67) {
+                                test.assertAnd(chunk.byteLength, 16000);
+                            }
+                            env.BDC.submitChunk(chunk);
+                        }, function () {
+                            // done
+                            test.assert(env.sender.getPosition(), env.totalChunks);
+                        });
+                    }
+                },
+                {
+                    desc: '# check receiver position number',
+                    run: function (env, test) {
+                        test.assert(env.receiver.getPosition(), 67);
+                    }
+                },
+                {
+                    desc: '# check receiver position number',
+                    run: function (env, test) {
+                        test.assert(env.receiver.getPosition(), 67);
+                    }
+                },
+                {
+                    desc: '# get file and get md5sum',
+                    run: function (env, test) {
+                        env.receiver.getFile(function (file, checksum) {
+                            test.assertAnd(file.byteLength, env.sender._arrayBuffer.byteLength);
+                            test.assert(checksum, env.sender.getChecksum());
+                        });
                     }
                 }
             ]
--- test/FileHelper.js
+++ test/FileHelper.js
@@ -1,12 +1,37 @@
-define(['fs'], function (fs) {
+if (typeof define !== 'function') {
+    console.log('typeof: ' + typeof require + ' :' + typeof define);
+  var define = require('amdefine')(module);
+}
+define([], function () {
 
     function FileHelper () {}
 
-    FileHelper.getFile = function (fileName) {
-        var data = fs.readFileSync('./test/'+fileName);
-        var ab = new ArrayBuffer(data.byteLength);
-        new Uint8Array(ab, 0, ab.byteLength).set(new Uint8Array(data));
-        return ab;
+    FileHelper.getFile = function (fileName, cb) {
+        if (typeof FileReader === 'undefined') {
+            var fs = require('fs');
+            var data = fs.readFileSync('./test/' + fileName);
+            var ab = new ArrayBuffer(data.byteLength);
+            new Uint8Array(ab, 0, ab.byteLength).set(new Uint8Array(data));
+            cb(ab);
+        } else {
+            fetch('test.mp4').then(function (res) {
+                return res.blob();
+            }).then(function (file) {
+                var reader = new FileReader();
+                reader.onload = function (event) {
+                    cb(event.target.result);
+                };
+
+                reader.onerror = function(event) {
+                    console.error("File could not be read! Code " + event.target.error.code);
+                };
+
+                reader.readAsArrayBuffer(file, '');  
+            }, function (err) {
+                console.log('err: ', err);
+            });
+            
+        }
     }
     
     return FileHelper;
