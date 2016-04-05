ec64a4bb23811631a5659250b3b9b0b2aefc6277
Nick Jennings <nick@silverbucket.net>
2016-04-05 16:38:38 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,15 @@ var baseDir = './src/';
 var baseFileName = 'index';
 var browserFileName = 'binary-data-chunking';
 var objName  = 'BinaryDataChunking';
+var targetDir = './browser';
 
 gulp.task('default', function () {
   var bundleStream = browserify(baseDir + baseFileName + '.js', { standalone: objName }).bundle();
 
   return bundleStream
          .pipe(source(browserFileName + '.js'))
-         .pipe(gulp.dest('./dist'))
+         .pipe(gulp.dest(targetDir))
          .pipe(streamify(uglify()))
          .pipe(rename(browserFileName + '.min.js'))
-         .pipe(gulp.dest('./dist'));
+         .pipe(gulp.dest(targetDir));
 });
